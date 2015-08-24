da2635f713985f3ed15c272658e874c21f0a3491
nidhi sharma <nidhi.sharma@metacube.com>
2015-08-24 18:38:21 +0530

    caseStudy1

---
+++ OOP/CaseStudy1/VehiclePrinter.java
@@ -0,0 +1,16 @@
+package session1;
+
+/**
+ * @author Nidhi
+ * Date : 24 august,2015
+ * Class to print the object details
+ * 
+ */
+public class VehiclePrinter {
+	public static String printVehicleSpecification(Vehicle vehicle) {
+
+		return vehicle.toString();// returning the object values
+
+	}
+
+}
