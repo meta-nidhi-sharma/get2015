deb8a884af4ce3983506641c2bd64715825ba5af
meta-nidhi-sharma <nidhi.sharma@metacube.com>
2015-09-03 10:42:42 +0530

    Update NameComparator.java

--- DS/NameComparator.java
+++ DS/NameComparator.java
@@ -1,15 +1,21 @@
 package EmployeesCollection;
+/**
+* @ author Nidhi Sharma
+* Date: 31 august,2015
+* Description: This class is comparing ans sorting employees based on their Name
+*/
 import java.util.Comparator;
 
 @SuppressWarnings("rawtypes")
-public class NameComparator implements Comparator {
-
+public class NameComparator implements Comparator 
+{
+	//Comparing objects of employees and sorting based on their names
 	@Override
-	public int compare(Object e1, Object e2) {
-		Employee em1 = (Employee) e1;
-		Employee em2 = (Employee) e2;
+	public int compare(Object employee1, Object employee2)
+	{
+		Employee em1 = (Employee) employee1;		//Holding details of employee 1
+		Employee em2 = (Employee)employee2;		//Holding details of employee 2
 		return (int)(em1.getEmployeeName().compareTo(em2.getEmployeeName()));
 	}
-
 }
 
