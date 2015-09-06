eaadc7fadbc0040b215c46935203b402af67e5d4
meta-nidhi-sharma <nidhi.sharma@metacube.com>
2015-09-07 00:28:54 +0530

    Update TreeSortTest.java

--- DS/TreeSortTest.java
+++ DS/TreeSortTest.java
@@ -27,8 +27,37 @@ public class TreeSortTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		BinaryTreeNode root = null;
+		BinaryTreeNode actualNode = TreeSort.insert(root, 6);
+		BinaryTreeNode expectedNode = new BinaryTreeNode(6);
+		assertEquals(expectedNode.nodeValue, actualNode.nodeValue);
+	}
+
+	@Test
+	public void test2() {
+		BinaryTreeNode root = null;
+		BinaryTreeNode node = TreeSort.insert(root, 6);
+		BinaryTreeNode actualNode = TreeSort.insert(node, 9);
+		BinaryTreeNode expectedNode = new BinaryTreeNode(node.nodeValue);
+		assertEquals(expectedNode.nodeValue, actualNode.nodeValue);
+	}
+	
+	@Test
+	public void test3() {
+		BinaryTreeNode root = null;
+		root = TreeSort.insert(root, 6);
+		root = TreeSort.insert(root, 9);
+		root = TreeSort.insert(root, 5);
+		root = TreeSort.insert(root, 3);
+		root = TreeSort.insert(root, 8);
+		root = TreeSort.insert(root, 5);
+		root = TreeSort.insert(root, 10);
+		root = TreeSort.insert(root, 0);
+		root = TreeSort.insert(root, 9);
+		String actual = TreeSort.treeSorting(root);
+		String expected = " 0 3 5 5 6 8 9 9 10";
+		assertEquals(expected, actual);
 	}
 
 }
