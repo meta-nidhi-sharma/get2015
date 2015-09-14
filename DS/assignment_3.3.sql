74161a5f719a65d71c7f9e3c55266716a92e0152
meta-nidhi-sharma <nidhi.sharma@metacube.com>
2015-09-14 10:40:26 +0530

    Update assignment_3.3.sql

--- DS/assignment_3.3.sql
+++ DS/assignment_3.3.sql
@@ -28,7 +28,7 @@ HAVING category='O'
 
 
 /*Display titles issued more than 2 times */
-SELECT DISTINCT titles.title_nm,books.purchase_dt,books.price
+SELECT DISTINCT titles.title_name,books.purchase_dt,books.price
 FROM 
 titles 
 INNER JOIN
@@ -52,8 +52,8 @@ COUNT(accession_no)>2
 
 
 /*Display books issued to category other than 'F' */
-SELECT book_issue.issue_dt,book_issue.accession_no,book_issue.member_id,book_issue.due_dt,
-members.member_nm,members.addressline1,members.addressline2,members.category
+SELECT book_issue.issue_date,book_issue.accession_no,book_issue.member_id,book_issue.due_dt,
+members.member_name,members.addressline1,members.addressline2,members.category
 FROM book_issue
 INNER JOIN
 members
@@ -63,7 +63,7 @@ WHERE members.category<>'F';
 
 
 /*Display authors for which at least one book have been purchased */
-SELECT DISTINCT authors.author_fn,authors.author_ln
+SELECT DISTINCT authors.author_name
 FROM authors 
 INNER JOIN 
 title_author 
