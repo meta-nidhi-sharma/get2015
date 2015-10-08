6552f18906060a7f4bf1b2c532e476f8b3bbee72
meta-nidhi-sharma <nidhi.sharma@metacube.com>
2015-10-08 11:57:39 +0530

    Update JavaScriptAssignment2.js

--- Web/JavaScriptAssignment2.js
+++ Web/JavaScriptAssignment2.js
@@ -1,43 +1,39 @@
-var index=5;
-
-function createRow()
-{	
-	var table=document.getElementById("table1");
-	var row=table.insertRow(table.rows.length);
+var index = 5;
+function createRow(){
 	
-	var cell1=row.insertCell(0);
-	var t1=document.createElement("input");
-		t1.type="name";
-		t1.id = "txtName"+index;
-		cell1.appendChild(t1);
-		
-	var cell2=row.insertCell(1);
-	var t2=document.createElement("input");
-		t2.type="number";
-		t2.id = "min"+index;
-		t2.min = "1";
-		t2.max = "10";
-		cell2.appendChild(t2);
-		
-	var cell3=row.insertCell(2);
-	var t3=document.createElement("input");
-		t3.type="number";
-		t3.id = "max"+index;
-		t3.min = "1";
-		t3.max = "10";
-		cell3.appendChild(t3);
+            var table=document.getElementById("table1");
+            var row=table.insertRow(table.rows.length);
+			
+            var cell1=row.insertCell(0);
+            var t1=document.createElement("input");
+			    t1.type="name";
+                t1.id = "txtName"+index;
+                cell1.appendChild(t1);
+				
+            var cell2=row.insertCell(1);
+            var t2=document.createElement("input");
+			    t2.type="number";
+                t2.id = "min"+index;
+				t2.min = "1";
+				t2.max = "10";
+                cell2.appendChild(t2);
+				
+            var cell3=row.insertCell(2);
+            var t3=document.createElement("input");
+			    t3.type="number";
+                t3.id = "max"+index;
+				t3.min = "1";
+				t3.max = "10";
+                cell3.appendChild(t3);
 	index++;
 }
-
-function removeRow()
-{
-	if(index > 1)
+function removeRow(){
+	if(index > 0)
 	{
     document.getElementById("table1").deleteRow(index);
 	index--;
 	}
-	else
-	{
+	else{
 		alert("Table is empty");
 	}
 }
@@ -54,55 +50,53 @@ function createGraph(){
 			var max = document.getElementById("max" + i).value;
 			var cell1=row.insertCell(0);
             var t1=document.createElement("input");
-			t1.type="name";
-			t1.value = document.getElementById("name" + i).value;
-			cell1.appendChild(t1);
+			    t1.type="name";
+                t1.value = document.getElementById("name" + i).value;
+                cell1.appendChild(t1);
 			for(var j=1; j<=10; j++)
 			{
 				 var cell2=row.insertCell(j);
-				var t2=document.createElement("input");
-				t2.style.width = '20px';
-				t2.id = "col"+i+j;
+			
+				cell2.style.width = '20px';
+				cell2.style.border="none";
 				if(j>=min && j<=max)
 				{
-					t2.style.backgroundColor="red";
+					cell2.style.backgroundColor="red";
 				}
-				cell2.appendChild(t2);
 			}
-		}		
+		}
 	}
 	else
 	{
 		alert("Cant create graph as table is empty");
-	}
-		document.getElementById("button1").disabled = true;	
+	}	
+	document.getElementById("button1").disabled = true;
+	document.getElementById("button2").disabled = true;
+	document.getElementById("button3").disabled = true;
+	document.getElementById("try1").disabled = true;
 }
 
 function validate()
 {
 	var flag= false;
-	for(var count=1; count<=index; count++)
-	{
+	for(var count=1; count<=index; count++){
 		var nam = document.getElementById("name" + count).value;
 		var minimum = document.getElementById("min" + count).value;
 		var maximum = document.getElementById("max" + count).value;
 			
 		if(minimum && maximum && nam)
 		{	
-			if(minimum<=maximum && minimum<=10 && maximum<=10 && minimum>=1 && maximum>=1)
+			if(minimum<=maximum)
 			{
 				flag=true;
 			}
 			else
 			{
-				flag=false;
-				alert("Enter valid values.");
-				break;
+				alert("minimum value is greter than max. Enter valid values.");
 			}
 		}
 		else
 		{
-			alert("All fields are mandatory");
 			flag=false;	
 			break;
 		}
@@ -111,4 +105,8 @@ function validate()
 	{
 		document.getElementById("button1").disabled = false;
 	}
+	else{
+		alert("All fields are mandatory");
+	}
+
 }
